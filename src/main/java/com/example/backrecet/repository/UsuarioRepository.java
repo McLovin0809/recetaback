ba33f34427ac47121cd205b7bf8273ecb8a3dad4
McLovin0809 <m.belmar@duocuc.cl>
2025-11-26 20:16:05 -0300

    Update UsuarioRepository.java

--- src/main/java/com/example/backrecet/repository/UsuarioRepository.java
+++ src/main/java/com/example/backrecet/repository/UsuarioRepository.java
@@ -1,3 +1,12 @@
+package com.example.backrecet.repository;
+
+import com.example.backrecet.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByEmail(String email);
